7e252cc4582890a32e9cf9ab4ef7bcc29b6ea472
Omer Jakobinsky <s0831911@sms.ed.ac.uk>
2011-07-14 19:07:11 +0100

    Mainly Rails 3 refactoring.
    - Updated the to use the nifty EachValidator
      syntax. The old syntax is still supported.
    - Added an option to :allow_nil

--- test/isbn_validation_test.rb
+++ test/isbn_validation_test.rb
@@ -2,96 +2,98 @@ require File.dirname(__FILE__) + '/test_helper'
 require File.dirname(__FILE__) + '/models'
 
 class IsbnValidationTest < Test::Unit::TestCase
-  def setup
-    @book = Book.new
-  end
-
   def test_isbn10_should_match_regex
     isbn = '1590599934'
-    assert isbn.match(Zerosum::ValidationExtensions::IsbnValidation::ISBN10_REGEX)
+    assert isbn.match(ValidationExtensions::IsbnValidation::ISBN10_REGEX)
   end
 
   def test_isbn10_should_not_match_regex
     isbn = 'abc123ab3344'
-    assert !isbn.match(Zerosum::ValidationExtensions::IsbnValidation::ISBN10_REGEX)
+    assert !isbn.match(ValidationExtensions::IsbnValidation::ISBN10_REGEX)
   end
 
   def test_isbn10_with_dashes_and_spaces_should_match_regex
     isbn = '159-059 9934'
-    assert isbn.match(Zerosum::ValidationExtensions::IsbnValidation::ISBN10_REGEX)
+    assert isbn.match(ValidationExtensions::IsbnValidation::ISBN10_REGEX)
   end
 
   def test_isbn13_should_match_regex
     isbn = '9781590599938'
-    assert isbn.match(Zerosum::ValidationExtensions::IsbnValidation::ISBN13_REGEX)
+    assert isbn.match(ValidationExtensions::IsbnValidation::ISBN13_REGEX)
   end
 
   def test_isbn13_should_not_match_regex
     isbn = '9991a9010599938'
-    assert !isbn.match(Zerosum::ValidationExtensions::IsbnValidation::ISBN13_REGEX)
+    assert !isbn.match(ValidationExtensions::IsbnValidation::ISBN13_REGEX)
   end
 
   def test_isbn13_with_dashes_and_spaces_should_match_regex
     isbn = '978-159059 9938'
-    assert isbn.match(Zerosum::ValidationExtensions::IsbnValidation::ISBN13_REGEX)
+    assert isbn.match(ValidationExtensions::IsbnValidation::ISBN13_REGEX)
   end
 
+
   def test_isbn10_should_pass_check_digit_verification
-    @book.isbn = '159059993-4'
-    assert @book.valid?
+    book = Book10.new
+    book.isbn = '159059993-4'
+    assert book.valid?
   end
 
   def test_isbn10_should_fail_check_digit_verification
-    @book.isbn = '159059993-0'
-    assert !@book.valid?
+    book = Book10.new
+    book.isbn = '159059993-0'
+    assert !book.valid?
   end
 
   def test_isbn10_should_handle_x_character_checksum
-    @book.isbn = '0-9722051-1-X'
-    assert @book.valid?
+    book = Book10.new
+    book.isbn = '0-9722051-1-X'
+    assert book.valid?
   end
 
+
   def test_isbn13_should_pass_check_digit_verification
-    @book.isbn = '978-1590599938'
-    assert @book.valid?
+    book = Book13.new
+    book.isbn = '978-1590599938'
+    assert book.valid?
   end
 
   def test_isbn13_should_fail_check_digit_verification
-    @book.isbn = '978-1590599934'
-    assert !@book.valid?
+    book = Book13.new
+    book.isbn = '978-1590599934'
+    assert !book.valid?
   end
 
-  def test_isbn_should_be_valid_if_either_isbn10_or_isbn13
-    @book.isbn = '978-1590599938'
-    assert @book.valid?
-    @book.isbn = '1590599934'
-    assert @book.valid?
+  def test_isbn13_with_zero_check_digit_should_validate
+    book = Book13.new
+    book.isbn = '978-1-60746-006-0'
+    assert book.valid?
   end
 
-  def test_isbn_should_validate_only_isbn10
-    @book = Book10.new
-    @book.isbn = '978-1590599938'
-    assert !@book.valid?
-    @book.isbn = '1590599934'
-    assert @book.valid?
-  end
 
-  def test_isbn_should_validate_only_isbn13
-    @book = Book13.new
-    @book.isbn = '1590599934'
-    assert !@book.valid?
-    @book.isbn = '978-1590599938'
-    assert @book.valid?
+  def test_isbn_should_be_valid_to_isbn10_by_default
+    book = Book.new
+    book.isbn = '1590599934'
+    assert book.valid?
   end
 
   def test_should_have_custom_error_message
-    @book.isbn = '978-159059AAAAAA'
-    @book.valid?
-    assert_equal 'is too fantastical!', @book.errors[:isbn].first
+    book = Book.new
+    book.isbn = '978-159059AAAAAA'
+    book.valid?
+    assert_equal 'is too fantastical!', book.errors[:isbn].first
   end
   
-  def test_isbn13_with_zero_check_digit_should_validate
-    @book.isbn = '978-1-60746-006-0'
-    assert @book.valid?
+  def test_blank_should_not_be_valid_by_default
+    book = Book13.new
+    book.isbn = ''
+    book.valid?
+    assert_equal 'is not a valid ISBN code', book.errors[:isbn].first
+  end
+  
+  def test_should_have_an_option_to_allow_nil
+    book = Book10.new
+    book.isbn = ''
+    assert book.valid?
   end
 end
